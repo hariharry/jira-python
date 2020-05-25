cd5bf3e71f61647505737241776599c920293c2f
U-HARI-OFFICE-PC\Dell <bigbasket.alerts@powerupcloud.com>
2020-05-25 16:28:48 +0530

    latest script

--- f1.py
+++ f1.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 import unicodedata
 import time
@@ -22,10 +23,10 @@ apikey=sys.argv[2]
 jenkinsuser=sys.argv[3]
 rw = sys.argv[4]
 instance = sys.argv[5]
-
-f = open("SSH_Access.log", "a")
+JiraTicketId =sys.argv[6]
+f = open("logs/SSH_Access.log", "a")
 sys.stdout = f
-
+print("{} Execution Begins".format(stime))
 #-----------------------------striping @example.com---------------------
 def stripMail(word):
 
@@ -36,8 +37,7 @@ def stripMail(word):
 def ExcelRead():
     try:
         #mentioned excel should have only read permision in jenkin server
-        os.system("aws s3 cp s3://bucketname/filename.xlsx  /var/lib/jenkins/workspace/ssh-access/filename.xlsx ")
-        df = pd.read_excel('filename.xlsx', sheet_name='sheet name')
+        df = pd.read_excel('publickeyssh.xlsx', sheet_name='public-ssh-keys-final')
         mailID = df['Email address']
 
         pkey = df['Public SSH Key']
@@ -55,17 +55,19 @@ def ExcelRead():
 
 
 #Priority list for Shift employees
-if (len(sys.argv)) < 7:
-    with open('mail_list.txt', 'r') as emails:
+
+os.system("aws s3 cp s3://sshaccess/publickeyssh.xlsx  /var/lib/jenkins/workspace/TestJiraSSHAccess/publickeyssh.xlsx ")
+os.system("aws s3 cp s3://sshaccess/PriorityMailList.txt  /var/lib/jenkins/workspace/TestJiraSSHAccess/PriorityMailList.txt ")
+if JiraTicketId == "no":
+    with open('PriorityMailList.txt', 'r') as emails:
         mail=emails.read()
         if re.search(jenkinsuser,mail):
-            print("found")
+            print("{} found".format(stime))
             pubkey=ExcelRead()
-            pubkey=str(pubkey)
             linuxusername=stripMail(jenkinsuser)
-            os.system("/var/lib/jenkins/workspace/ssh-access/myshell.sh %s admin '%s' fe '%s' 2"% (linuxusername,pubkey,instance))
-            time.sleep(60)
-            print("{} Build user {} is in priority list".format(stime,jenkinsuser))
+            os.system("/var/lib/jenkins/workspace/TestJiraSSHAccess/useradd.sh %s admin '\"%s\"' fe '%s' 1"% (linuxusername,pubkey,instance))
+            time.sleep(30)
+            print("{} Build user {} is in priority list and given access for {}".format(stime,jenkinsuser,instance))
         else :
             print("{} Build user {} is not in priority list".format(stime,jenkinsuser))
             print("Please provide valid JiraTicketId")
@@ -75,7 +77,7 @@ if (len(sys.argv)) < 7:
 
 #If build user is not in priority list
 else:
-    JiraTicketId =sys.argv[6]
+    #jira url
     server = 'https://example.atlassian.net'
     options = { 'server': server }
 
@@ -97,8 +99,7 @@ else:
     def ExceljiraRead():
         try:
             #mentioned excel should have only read permision in jenkin server
-            os.system("aws s3 cp s3://bucketname/filename.xlsx  /var/lib/jenkins/workspace/ssh-access/filename.xlsx ")
-            df = pd.read_excel('filename.xlsx', sheet_name='public-ssh-keys')
+            df = pd.read_excel('publickeyssh.xlsx', sheet_name='public-ssh-keys-final')
             jiraDisplayName = df['Employee Name']
             jmailID = df['Email address']
             for i in range(0,len(jmailID)-1):
@@ -112,21 +113,23 @@ else:
 
 
     #Checking the issue type,severity and status of the ticket
-    if((str(ticketType))=="Bug" and (str(getvalue))=="S1" and (str(status))=="Open"):
+    if((str(ticketType))=="Bug" and (str(getvalue))=="S1" and (str(status))=="Open in Engineering"):
                     jiramailID=str(ExceljiraRead())
                     pubkey=str(ExcelRead())
                     if jiramailID==jenkinsuser:
                         print("{} Jira ticket assignee and username is matching".format(stime))
                         if rw=='RO':
-                            print('{} Read Only access have given for {} in {} server '.format(stime,jenkinsuser,instance))
+                            print('{} Read Only access have given for {} in {} server for ticket {}  '.format(stime,jenkinsuser,instance,JiraTicketId))
                             linuxusername=stripMail(jiramailID)
-                            os.system("/var/lib/jenkins/workspace/ssh-access/myshell.sh %s admin '%s' fe '%s' 2"% (linuxusername,pubkey,instance))
+                            os.system("/var/lib/jenkins/workspace/TestJiraSSHAccess/useradd.sh %s admin '\"%s\"' fe '%s' 1"% (linuxusername,pubkey,instance))
                         else:
-                            print('{} Admin access have given for {} in {} server '.format(stime,jenkinsuser,instance))
+                            print('{} Admin access have given for {} in {} server for ticket {} '.format(stime,jenkinsuser,instance,JiraTicketId))
                             linuxusername=stripMail(jiramailID)
-                            os.system("/var/lib/jenkins/workspace/ssh-access/myshell.sh %s read '%s' fe '%s' 2"% (linuxusername,pubkey,instance))
-                          
+                            os.system("/var/lib/jenkins/workspace/TestJiraSSHAccess/useradd.sh %s read '\"%s\"' fe '%s' 2"% (linuxusername,pubkey,instance))
+                       f.close()
     else:
         print('{} Something went wrong please contact administrator'.format(stime))
         print("Possible cases :\n\t issuetype not bug \n\t severity is not S1 \n\t ticket status might be changed \n\t Given mail id is wrong")
 f.close()
+time.sleep(20)
+os.system("rm -rf /var/lib/jenkins/workspace/TestJiraSSHAccess/publickeyssh.xlsx /var/lib/jenkins/workspace/TestJiraSSHAccess/PriorityMailList.txt")
